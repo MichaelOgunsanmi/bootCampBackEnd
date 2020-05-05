a40fee751fe724bc4a7cc933829b13fe61d636d3
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-05-05 14:08:19 +0100

    Added up the user route and included necessary controllers to be implemented

---
+++ routes/userRoute.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    authenticateUser,
+    authorizeUser,
+    validateUserRequestBody,
+    uploadUserPhoto,
+    resizeUserPhoto,
+    getCurrentUser,
+    filterRequestQueryObject
+} = require('../middlewares');
+
+
+const {
+    getSingleUserController,
+    getAllUsersController,
+    updateUserController,
+    deleteUserController,
+    adminDeleteUserController
+} = require('../controllers/user');
+
+
+router.use(authenticateUser);
+
+router.patch('/updateMyPassword', validateUserRequestBody, updatePasswordController);
+router.get('/me', getCurrentUser, getSingleUserController);
+router.patch('/updateMe', uploadUserPhoto, resizeUserPhoto, validateUserRequestBody, updateUserController);
+router.delete('/deleteMe', deleteUserController);
+
+router.use(authorizeUser('admin'));
+
+router
+    .route('/:id')
+    .get(getSingleUserController)
+    .delete(adminDeleteUserController);
+
+router
+    .route('/')
+    .get(filterRequestQueryObject, getAllUsersController);
+
+
+
+module.exports = router;
